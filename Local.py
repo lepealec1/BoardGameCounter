95e9d8652fce568596576128b03b33e7f85ef5b2
lepealec2 <117782443+lepealec2@users.noreply.github.com>
2026-04-13 11:32:45 -0700

    Update Local.py

--- Local.py
+++ Local.py
@@ -114,7 +114,7 @@ for name, data in st.session_state.counters.items():
         )
 
         # BUTTON ROW (TRUE STREAMLIT LAYOUT)
-        c1, c2, c3 = st.columns(3)
+        c1, c2, c3 = st.columns(3,gap="xxsmall")
 
         with c1:
             if st.button("➖", key=f"dec_{name}"):
